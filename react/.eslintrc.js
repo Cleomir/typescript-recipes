f5a16895f6d1b1d5f12d5f44c98279ef47115098
Cleomir <cleomir.mg@hotmail.com>
2020-06-05 08:56:10 +0300

    Add Eslint

---
+++ react/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "jest"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:jest/recommended",
+    "plugin:react/recommended",
+  ],
+  env: {
+    node: true,
+  },
+  rules: {
+    "@typescript-eslint/no-var-requires": 0,
+  },
+};
