f2fbbd21481721fbd066b741baac28ea05ea619f
Cleomir <cleomir.mg@hotmail.com>
2020-06-21 14:00:57 +0300

    Add documentation

--- express-boilerplate/src/config/envVariables.ts
+++ express-boilerplate/src/config/envVariables.ts
@@ -12,6 +12,9 @@ export const env: { [key: string]: string } = {
   SERVER_PORT: process.env.SERVER_PORT!,
 };
 
+/**
+ * Check if all env variables are defined, otherwise an error is thrown
+ */
 export const checkForUndefinedEnvVariables = (): void => {
   for (const key in env) {
     if (!env[key]) {
