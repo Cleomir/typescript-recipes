f064492e24f594b236c2ecf08f3169a85d4cf949
Cleomir <cleomir.mg@hotmail.com>
2020-06-30 09:22:11 +0300

    Add merged interface

---
+++ express-boilerplate/src/interfaces/merged/Request.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Request } from "express";
+
+declare global {
+  namespace Express {
+    export interface Request {
+      id: string;
+    }
+  }
+}
