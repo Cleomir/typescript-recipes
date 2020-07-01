7c9e1998802a86ada414fb3342cf4d4ef9ab893c
Cleomir <cleomir.mg@hotmail.com>
2020-07-01 09:31:37 +0300

    Refactor implementation

--- rabbitmq/connection.ts
+++ rabbitmq/connection.ts
@@ -1,23 +1,13 @@
-import amqp, { Connection } from "amqplib";
+import amqp, { AmqpConnectionManager } from "amqp-connection-manager";
 
-export default class RabbitMQ {
-  private static connection: Connection | undefined;
+let connection: AmqpConnectionManager | undefined;
 
-  public static async getConnection(): Promise<Connection | undefined> {
-    try {
-      if (!this.connection) {
-        this.connection = await amqp.connect(
-          `amqp://user:password@localhost:?heartbeat=60`
-        );
-      }
-
-      this.connection.on("close", () => {
-        setTimeout(this.getConnection, 1000);
-      });
-
-      return this.connection;
-    } catch (error) {
-      setTimeout(this.getConnection, 1000);
-    }
+const getConnection = async (): Promise<AmqpConnectionManager> => {
+  if (!connection) {
+    connection = amqp.connect([`amqp://username:password@localhost`]);
   }
-}
+
+  return connection;
+};
+
+export default getConnection;
