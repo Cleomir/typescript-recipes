2d5e0caf2083933f35d5783f1f88efee67d1d811
Cleomir <cleomir.mg@hotmail.com>
2020-11-15 10:17:13 +0200

    Fix syntax

--- express-boilerplate/webpack.dev.js
+++ express-boilerplate/webpack.dev.js
@@ -1,7 +1,4 @@
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
