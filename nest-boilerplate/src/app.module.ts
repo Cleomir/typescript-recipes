c566e2accbd3595549fe6d01b6b815d018db57ba
Cleomir <cleomir.mg@hotmail.com>
2020-12-08 10:15:24 +0200

    Add mongo connection config

--- nest-boilerplate/src/app.module.ts
+++ nest-boilerplate/src/app.module.ts
@@ -1,19 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import appEnvConfig from './app.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
+import { LoggerService } from './logger/logger.service';
 import { RequestLoggerModule } from './request-logger/request-logger.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [appEnvConfig], isGlobal: true }),
+    MongooseModule.forRoot('mongodb://localhost:27017/db'),
     LoggerModule,
     RequestLoggerModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, LoggerService],
 })
 export class AppModule {}
